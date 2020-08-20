78033613e9f1ee20854b7317c5632b1767d6e3ce
jiangyu <jiangyu53231323@163.com>
2020-08-20 11:48:57 +0800

    二进制读写

---
+++ 04BinaryIO/04BinaryIO.cpp
@@ -0,0 +1,38 @@
+﻿// 04BinaryIO.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <array>
+
+int main()
+{
+    namespace fs = std::filesystem;
+    using io = std::ios;
+
+    fs::path p{ "array.dat" };
+    std::fstream out{ p,io::out | io::app };
+
+    std::array a{ 21L,42L,63L };
+    std::streamsize size = a.size() * sizeof(a[0]);
+    out.write(reinterpret_cast<char*>(&a[0]), size);
+
+    out.close();
+    out.open(p, io::in);
+
+    auto x{ 0L };
+    out.read(reinterpret_cast<char*>(&x), sizeof(x));
+    std::cout << x << std::endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
